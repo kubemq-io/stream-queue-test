6a52c8b40c40908ab82a6aad05b22aa9ed487f97
kubemq <lior.nabat@kubemq.io>
2021-04-25 05:39:51 +0300

    update with new SDK

--- main.go
+++ main.go
@@ -14,23 +14,21 @@ import (
 )
 
 type Config struct {
-	Address   string
-	Queue     string
-	Send      int
-	Threads   int
-	Rounds    int
-	AckDelay  int
-	Receivers int
+	Address  string
+	Queue    string
+	Send     int
+	Threads  int
+	Rounds   int
+	AckDelay int
 }
 
 var (
 	_ = pflag.String("address", "localhost", "kubemq-address")
-	_ = pflag.String("queue", "q1", "queue destination")
+	_ = pflag.String("queue", "d", "queue destination")
 	_ = pflag.Int("send", 1000, "total send messages")
-	_ = pflag.Int("threads", 1, "total threads")
+	_ = pflag.Int("threads", 10, "total threads")
 	_ = pflag.Int("rounds", 0, "total rounds")
-	_ = pflag.Int("ack_delay", 10, "ack delay in seconds")
-	_ = pflag.Int("receivers", 1, "num of receivers for each threads")
+	_ = pflag.Int("ack_delay", 0, "ack delay in seconds")
 )
 
 func LoadConfig() (*Config, error) {
@@ -44,7 +42,6 @@ func LoadConfig() (*Config, error) {
 	_ = viper.BindEnv("Queue", "QUEUE")
 	_ = viper.BindEnv("Rounds", "ROUNDS")
 	_ = viper.BindEnv("AckDelay", "ACK_DELAY")
-	_ = viper.BindEnv("Receivers", "RECEIVERS")
 
 	_ = viper.BindPFlag("Address", pflag.CommandLine.Lookup("address"))
 	_ = viper.BindPFlag("Queue", pflag.CommandLine.Lookup("queue"))
@@ -52,7 +49,6 @@ func LoadConfig() (*Config, error) {
 	_ = viper.BindPFlag("Threads", pflag.CommandLine.Lookup("threads"))
 	_ = viper.BindPFlag("Rounds", pflag.CommandLine.Lookup("rounds"))
 	_ = viper.BindPFlag("AckDelay", pflag.CommandLine.Lookup("ack_delay"))
-	_ = viper.BindPFlag("Receivers", pflag.CommandLine.Lookup("receivers"))
 
 	err := viper.Unmarshal(cfg)
 	if err != nil {
@@ -68,6 +64,13 @@ func getFloatAvg(list []float64) float64 {
 	}
 	return cnt / float64(len(list))
 }
+func getClient(ctx context.Context, host string, port int) *kubemq.QueuesClient {
+	c, _ := kubemq.NewQueuesClient(ctx,
+		kubemq.WithAddress(host, port),
+		kubemq.WithClientId("stream-queue-sender"),
+		kubemq.WithTransportType(kubemq.TransportTypeGRPC))
+	return c
+}
 func main() {
 	cfg, err := LoadConfig()
 	rand.Seed(time.Now().UnixNano())
@@ -76,15 +79,18 @@ func main() {
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	sender, err := kubemq.NewQueuesClient(ctx,
-		kubemq.WithAddress(cfg.Address, 50000),
-		kubemq.WithClientId("stream-queue-sender"),
-		kubemq.WithTransportType(kubemq.TransportTypeGRPC))
-	if err != nil {
-		log.Fatal(err)
+	var senders []*kubemq.QueuesClient
+	senders = append(senders,
+		getClient(ctx, cfg.Address, 50000),
+		getClient(ctx, cfg.Address, 50000),
+		getClient(ctx, cfg.Address, 50000),
+	)
 
-	}
-	defer sender.Close()
+	defer func() {
+		for i := 0; i < len(senders); i++ {
+			_ = senders[i].Close()
+		}
+	}()
 	var roundList []float64
 	cnt := 0
 	for {
@@ -99,7 +105,7 @@ func main() {
 				batch = append(batch, kubemq.NewQueueMessage().
 					SetChannel(channel).SetBody([]byte("some-stream-simple-queue-message")))
 			}
-			_, err = sender.Batch(ctx, batch)
+			_, err = senders[i%len(senders)].Batch(ctx, batch)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -115,6 +121,7 @@ func main() {
 				cnt := atomic.NewInt32(0)
 				ctx, cancel := context.WithCancel(context.Background())
 				defer cancel()
+
 				receiver, err := kubemq.NewQueuesClient(ctx,
 					kubemq.WithAddress(cfg.Address, 50000),
 					kubemq.WithClientId(fmt.Sprintf("stream-queue-receiver-%d", index)),
@@ -125,37 +132,48 @@ func main() {
 
 				}
 				defer receiver.Close()
-				for i := 0; i < cfg.Receivers; i++ {
-					go func(ctx context.Context, index int) {
-						channel := fmt.Sprintf("%s%d", cfg.Queue, index)
-						done, err := receiver.TransactionStream(ctx, kubemq.NewQueueTransactionMessageRequest().SetChannel(channel).SetVisibilitySeconds(10).SetWaitTimeSeconds(15), func(response *kubemq.QueueTransactionMessageResponse, err error) {
-							if err != nil {
+				channel := fmt.Sprintf("%s%d", cfg.Queue, index)
+				done, err := receiver.TransactionStream(ctx, kubemq.NewQueueTransactionMessageRequest().SetChannel(channel).SetVisibilitySeconds(10).SetWaitTimeSeconds(15), func(response *kubemq.QueueTransactionMessageResponse, err error) {
+					if err != nil {
 
-							} else {
-								cnt.Inc()
-								//log.Println (fmt.Sprintf("queue: %s, message seq %d received",response.Message.Channel,response.Message.Attributes.Sequence))
-								//t:=rand.Intn(1200-200+1) + 200
-								//time.Sleep(time.Duration(t)  *time.Millisecond)
-								time.Sleep(time.Duration(cfg.AckDelay) * time.Millisecond)
-								err = response.Ack()
-								if err != nil {
-									//log.Fatal(err)
-								}
-
-							}
-						})
+					} else {
+						cnt.Inc()
+						//log.Println (fmt.Sprintf("queue: %s, message seq %d received",response.Message.Channel,response.Message.Attributes.Sequence))
+						//t:=rand.Intn(1200-200+1) + 200
+						//time.Sleep(time.Duration(t)  *time.Millisecond)
+						time.Sleep(time.Duration(cfg.AckDelay) * time.Millisecond)
+						err = response.Ack()
 						if err != nil {
-							return
+							//log.Fatal(err)
 						}
-						<-ctx.Done()
-						done <- struct{}{}
-					}(ctx, index)
-				}
+
+					}
+				})
+
+				//localctx, localCancel := context.WithCancel(ctx)
+				//for i := 0; i < 10; i++ {
+				//	go func() {
+				//		done, err := receiver.Subscribe(localctx, kubemq.NewReceiveQueueMessagesRequest().SetChannel(channel).SetMaxNumberOfMessages(cfg.Send/10).SetWaitTimeSeconds(60), func(response *kubemq.ReceiveQueueMessagesResponse, err error) {
+				//			if err != nil {
+				//
+				//			} else {
+				//				cnt.Add(response.MessagesReceived)
+				//			}
+				//		})
+				//		if err != nil {
+				//			return
+				//		}
+				//		<-localctx.Done()
+				//		done <- struct{}{}
+				//	}()
+				//
+				//}
 
 				for {
 					select {
 					case <-time.After(1 * time.Millisecond):
 						if cnt.Load() >= int32(cfg.Send) {
+							done <- struct{}{}
 							return
 						}
 					}
